1f3e354cc8b4a217d436d40d1f2365a9771ddbe4
JustAn0therDev <ruanmontelo@gmail.com>
2020-10-15 02:34:39 -0300

    Changing the strutcture of the code to be more object-oriented and depending only on a configuration file

--- mail_handling_tests.py
+++ mail_handling_tests.py
@@ -1,12 +1,22 @@
 import unittest
 from config import Config
+from imap_connection_handler import ImapConnectionHandler
 
 
 class MyTestCase(unittest.TestCase):
-    def test_config(self):
-        config = Config()
+    def test_that_no_obligatory_config_prop_is_empty(self):
+        config = Config('configs.txt')
         self.assertTrue(config.email is not None)
-        self.assertTrue(config.password is not None)
+        self.assertTrue(config.app_password is not None)
+        self.assertTrue(config.search is not None)
+        self.assertTrue(config.data_to_look_for is not None)
+        self.assertTrue(config.mailbox is not None)
+        self.assertTrue(config.imap_address is not None)
+
+    def test_imap_connection_handler_instance(self):
+        config = Config('configs.txt')
+        imap_connection_handler = ImapConnectionHandler(config)
+        self.assertTrue(imap_connection_handler is not None)
 
 
 if __name__ == '__main__':
